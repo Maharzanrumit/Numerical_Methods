a6f207a4918853e58c57e4a66477992f9bb04c97
Rumit Maharjan <rumitmaharjan4@gmail.com>
2025-03-08 09:22:19 +0545

    Add files via upload

---
+++ Lab_05/Euler's_Hune's_RK4_method.cpp
@@ -0,0 +1,73 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+float fun(float a, float b){
+    float result1=0;
+    result1=a+b;
+    return result1;
+}
+void Rk4(float h,float x,float y,float xn){
+     float m1,m2,m3,m4;
+     int i=0;
+    
+    cout<<"\n\nRunge Kutta Method\n";
+    while(x !=xn){
+        m1=fun(x,y);
+        m2=fun(x+(h/2),y+(m1*(h/2)));
+        m3=fun(x+(h/2),y+(m2*(h/2)));
+        m4=fun(x+h,y+(m3*(h/2)));
+        y=y+(m1+(2*m2)+(2*m3)+m4)*(h/6);
+        cout<<"Itr :"<<i++<<endl;
+        cout<<"y("<<x<<") : "<<setprecision(3)<<y;
+        cout<<endl;
+        x +=h;
+    }
+}
+
+void Euler(float h, float x, float y,float xn){
+	int i=1;
+	cout<<"\n\nEulers Method\n";
+	while(x !=xn){
+		y=y+(h*fun(x,y));
+		cout<<"Itr :"<<i++<<endl;
+        cout<<"y("<<x<<") : "<<setprecision(3)<<y;
+        cout<<endl;
+		x+=h;
+	}
+}
+
+void Hune(float h, float x, float y, float xn){
+	float m1,m2 ;
+	int i=0;
+	cout<<"\n\nHunes Method\n";
+	while(x !=xn){
+		m1=fun(x,y);
+		m2=fun(x+h,y+(m1*h));
+		y=y+((m1+m2)*(h/2.0));
+		cout<<"Itr :"<<i++<<endl;
+		cout<<"y("<<x<<") : "<<setprecision(3)<<y;
+        cout<<endl;
+		x+=h;
+	}
+}
+
+
+int main() {
+    float h,x,y,xn;
+   
+    cout<<"Enter the value for h :";
+    cin>>h;
+    cout<<"Initial value for x and y :";
+    cin>>x>>y;
+    cout<<"Final value for X : ";
+    cin>>xn;
+    
+    Euler(h,x,y,xn);
+    Hune(h,x,y,xn);
+    Rk4(h,x,y,xn);
+    
+    
+    return 0;
+}
